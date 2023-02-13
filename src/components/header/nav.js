deffb4de9f3f0442ede0c966653ec8d09676ffea
rahulmishra <rahulm@rahulmishras-MacBook-Air.local>
2023-02-13 21:52:12 +0530

    adding nav component

--- src/components/header/nav.js
+++ src/components/header/nav.js
@@ -1,7 +1,38 @@
+import './nav.css';
+import AppBar from '@mui/material/AppBar';
+import { Container } from '@mui/system';
+import Typography from '@mui/material/Typography';
+import { Avatar } from '@mui/material';
+
 function nav() {
-    return ( <div>
-        Nav bar component are build
-    </div> );
+  return (<>
+    <AppBar position='relative'>
+      <Container maxWidth="xl">
+        <Typography
+          noWrap
+          component="a"
+          href="/"
+          sx={{
+            mr: 2,
+            display: { xs: 'none', md: 'flex' },
+            fontWeight: 700,
+            color: '#FFFFFF',
+            textDecoration: 'none',
+            lineHeight: 3
+          }}
+        >
+          <div className="icon-store">
+            <div>
+              <Avatar alt="Remy Sharp" src="../reddit.png" />
+            </div>
+            <div>
+              <h3 sx={{ color: '#000000' }}> reddit</h3>
+            </div>
+          </div>
+        </Typography>
+      </Container>
+    </AppBar>
+  </>);
 }
 
-export default nav;
+export default nav; 
