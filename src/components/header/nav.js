41bc024c7eaaf547b9eccdfc463d7e8ed63cc672
rahulmishra <rahulm@rahulmishras-MacBook-Air.local>
2023-02-14 22:19:04 +0530

    updating nav

--- src/components/header/nav.js
+++ src/components/header/nav.js
@@ -3,6 +3,8 @@ import AppBar from '@mui/material/AppBar';
 import { Container } from '@mui/system';
 import Typography from '@mui/material/Typography';
 import { Avatar } from '@mui/material';
+import TextField from '@mui/material/TextField';
+import FormControl from '@mui/material/FormControl';
 
 function nav() {
   return (<>
@@ -11,7 +13,6 @@ function nav() {
         <Typography
           noWrap
           component="a"
-          href="/"
           sx={{
             mr: 2,
             display: { xs: 'none', md: 'flex' },
@@ -29,10 +30,39 @@ function nav() {
               <h3 sx={{ color: '#000000' }}> reddit</h3>
             </div>
           </div>
+          <div className='input-home'>
+            <TextField
+              defaultValue="Home"
+              size="small"
+              sx={{ width: 250 }}
+            />
+          </div>
+          <div className='input-search-section'>
+            <FormControl fullWidth sx={{m:0.5}} variant="filled">
+              <TextField
+              defaultValue="Search"
+              size="small"
+              sx={{ width: 450 }}
+            />
+            </FormControl>
+          </div>
+          <div className='icon-details'>
+            <Avatar
+            sx={{ width: 24, height: 24 }} 
+            src='../dollar-coin.png'></Avatar>
+          </div>
+          <div className='icon-with-notification'>
+            <Avatar
+            sx={{ width: 24, height: 24 }}
+            src='../messenger.png'></Avatar>
+            <Avatar  sx={{ width: 24, height: 24 }}src='../bell.png'></Avatar>
+            <Avatar sx={{ width: 24, height: 24 }} src='../plus.png'></Avatar>
+            <Avatar sx={{ width: 24, height: 24 }} src='../megaphone.png'></Avatar>
+          </div>
         </Typography>
       </Container>
     </AppBar>
   </>);
 }
 
-export default nav; 
+export default nav;  
